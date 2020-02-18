7d81801307306f31f091c71100b24bd96bf75556
Bill YENGO <yengobilly4063@gmail.com>
2020-02-18 13:50:19 +0200

    DI(dependency injection) implemented

---
+++ BlazorMovies/Client/Helpers/Repository.cs
@@ -0,0 +1,22 @@
+﻿using BlazorMovies.Shared.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorMovies.Client.Helpers
+{
+    public class Repository : IRepository
+    {
+
+        public List<Movie> GetMovies()
+        {
+            return new List<Movie>()
+            {
+            new Movie(){Title="The Avengers", ReleaseDate=new DateTime(2020, 1, 23)},
+            new Movie(){Title="Apider-Man: Home Coming", ReleaseDate=new DateTime(2009, 5, 15)},
+            new Movie(){Title="Moana", ReleaseDate=new DateTime(2010, 10, 21)},
+            };
+        }
+    }
+}
--- BlazorMovies/Client/Program.cs
+++ BlazorMovies/Client/Program.cs
@@ -4,6 +4,7 @@ using System.Threading.Tasks;
 using System.Text;
 using Microsoft.AspNetCore.Blazor.Hosting;
 using Microsoft.Extensions.DependencyInjection;
+using BlazorMovies.Client.Helpers;
 
 namespace BlazorMovies.Client
 {
@@ -13,8 +14,19 @@ namespace BlazorMovies.Client
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("app");
+            ConfigurServices(builder.Services);
 
             await builder.Build().RunAsync();
         }
+
+        private static void ConfigurServices(IServiceCollection services)
+        {
+            services.AddOptions();      //Authorization System
+            services.AddSingleton<SingletonService>();
+            services.AddTransient<TransientService>();
+            //get Movies services
+            services.AddTransient<IRepository, Repository>();
+        }
+
     }
 }
