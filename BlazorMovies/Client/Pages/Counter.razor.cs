c60487ca5db0f43b247aab98241eca8745f166ea
Bill YENGO <yengobilly4063@gmail.com>
2020-02-18 15:20:32 +0200

    js in dotnet invocation

---
+++ BlazorMovies/Client/Pages/Counter.razor.cs
@@ -0,0 +1,56 @@
+﻿using BlazorMovies.Shared.Entities;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorMovies.Client.Pages
+{
+    public partial class Counter
+    {
+
+        [Inject] SingletonService singleton { get; set; }
+        [Inject] TransientService transient { get; set; }
+        [Inject] IJSRuntime js { get; set; }
+
+        private int currentCount = 0;
+        private static int currentCountStatic = 0;
+
+        [JSInvokable]
+        public async Task IncrementCount()
+        {
+            currentCount++;
+            transient.Value = currentCount;
+            singleton.Value = currentCount;
+            currentCountStatic++;
+            await js.InvokeVoidAsync("dotnetStaticInvocation");
+        }
+
+        private async Task IncrementCountJavascript()
+        {
+            await js.InvokeVoidAsync("dotnetInstanceInvocation", 
+                DotNetObjectReference.Create(this)
+                );
+        }
+
+
+
+        public List<Movie> movies;
+
+        protected override void OnInitialized()
+        {
+            movies = new List<Movie>()
+        {
+            new Movie(){Title = "The Rock", ReleaseDate=new DateTime(2008, 12, 24)}
+        };
+        }
+
+        [JSInvokable]
+        public static Task<int> GetCurrentCount()
+        {
+            return Task.FromResult(currentCountStatic);
+        }
+    }
+}
---
+++ BlazorMovies/Client/wwwroot/js/Utilities.js
@@ -0,0 +1,14 @@
+﻿function my_function(message) {
+    console.log("From utilities " + message);
+}
+
+function dotnetStaticInvocation() {
+    DotNet.invokeMethodAsync("BlazorMovies.Client", "GetCurrentCount")
+        .then(result => {
+            console.log("count from javascript " + result )
+        })
+}
+
+function dotnetInstanceInvocation(dotnetHelper) {
+    dotnetHelper.invokeMethodAsync("IncrementCount");
+}
